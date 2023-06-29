3e49c4b87cc1e1c0188d53f3a54ec3ce0bf24f0b
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-06-29 13:58:18 +0100

    add verse rendering

--- src/views/HymnView.vue
+++ src/views/HymnView.vue
@@ -14,9 +14,34 @@ export default {
 
 <template>
   <div>
-    <h1>{{ hymnData.number }}. {{ hymnData.title}}</h1>
-    <div v-for="verse in hymnData.verses" :key="verse.id">
-      <p>{{verse}}</p>
+    <h1>{{ hymnData.number }}. {{ hymnData.title }}</h1>
+    <div>
+      <span v-for="line in hymnData.verses[0]" :key="line.id">
+        {{ line }}
+        <!-- TODO: remove breakline -->
+        <br>
+      </span>
+      <br>
+    </div>
+    <div>
+      <span>Priedainis:</span>
+      <br>
+      <span v-for="line in hymnData.chorus" :key="line.id">
+        {{ line }}
+        <!-- TODO: remove breakline -->
+        <br>
+      </span>
+      <br>
+    </div>
+    <br>
+    <div v-for="verse in hymnData.verses.slice(1)" :key="verse.id">
+      <span v-for="line in verse" :key="line.id">
+        {{ line }}
+        <!-- TODO: remove breakline -->
+        <br>
+      </span>
+      <!-- TODO: remove breakline -->
+      <br>
     </div>
   </div>
-</template>
+</template>        
