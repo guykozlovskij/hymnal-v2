2149d0101e6ba1e55582815a2355dff280c7fa7a
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-07-04 20:31:41 +0100

    add conditional proper styling

--- src/views/HymnView.vue
+++ src/views/HymnView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import json from '../data/hymnal-data.json'
+
 </script>
 
 <script>
@@ -15,27 +16,29 @@ export default {
 <template>
   <section class="hymn-view">
     <h1>{{ hymnData.number }}. {{ hymnData.title }}</h1>
-    <ol class="verse-one">
-      <li class="hymn-verse">
-        <span v-for="line in hymnData.verses[0]" :key="line.id">
+    <div>
+      <ol class="verse-one">
+        <li class="hymn-verse">
+          <span v-for="line in hymnData.verses[0]" :key="line.id">
+            {{ line }}
+            <br>
+          </span>
+        </li>
+      </ol>
+      <div v-if="hymnData.chorus" class="hymn-chorus">
+        <span v-for="line in hymnData.chorus" :key="line.id">
           {{ line }}
           <br>
         </span>
-      </li>
-    </ol>
-    <div class="hymn-chorus">
-      <span v-for="line in hymnData.chorus" :key="line.id">
-        {{ line }}
-        <br>
-      </span>
+      </div>
+      <ol class="remaining-verses" start="2">
+        <li class="hymn-verse" v-for="verse in hymnData.verses.slice(1)" :key="verse.id">
+          <span v-for="line in verse" :key="line.id">
+            {{ line }}
+            <br>
+          </span>
+        </li>
+      </ol>
     </div>
-    <ol class="remaining-verses" start="2">
-      <li class="hymn-verse" v-for="verse in hymnData.verses.slice(1)" :key="verse.id">
-        <span v-for="line in verse" :key="line.id">
-          {{ line }}
-          <br>
-        </span>
-      </li>
-    </ol>
   </section>
 </template>        
