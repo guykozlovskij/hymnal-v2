d858ea44f369eb851f3ed1ccb381b5365034b7e1
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-07-04 22:42:03 +0100

    add indeces as keys

--- src/views/HymnView.vue
+++ src/views/HymnView.vue
@@ -15,7 +15,6 @@ export default {
 
 <template>
   <section class="hymn-view">
-    <!-- TODO investigate IDs-->
     <div id="hymn-intro">
       <h4>{{ hymnData.number }}. {{ hymnData.title }}</h4>
       <h4 v-if="hymnData['sub-title']">{{ hymnData['sub-title'] }}</h4>
@@ -24,20 +23,20 @@ export default {
     <div id="verses">
       <ol class="verse-one">
         <li class="hymn-verse">
-          <span v-for="line in hymnData.verses[0]" :key="line.id">
+          <span v-for="(line, index) in hymnData.verses[0]" :key="index">
             {{ line }}
             <br>
           </span>
         </li>
       </ol>
       <div v-if="hymnData.chorus" class="hymn-chorus">
-        <span v-for="line in hymnData.chorus" :key="line.id">
+        <span v-for="(line, index) in hymnData.chorus" :key="index">
           {{ line }}
           <br>
         </span>
       </div>
       <ol class="remaining-verses" start="2">
-        <li class="hymn-verse" v-for="verse in hymnData.verses.slice(1)" :key="verse.id">
+        <li class="hymn-verse" v-for="(verse, index) in hymnData.verses.slice(1)" :key="index">
           <span v-for="line in verse" :key="line.id">
             {{ line }}
             <br>
