1e330c48b8feeda172c0cf1d9f97181c6386be4a
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-07-05 18:52:10 +0100

    add new hymns, hymnal.net url and dinamyc melody pathways

--- src/views/HymnView.vue
+++ src/views/HymnView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import json from '../data/hymnal-data.json'
-
 </script>
 
 <script>
@@ -20,8 +19,8 @@ export default {
       <h4 v-if="hymnData['sub-title']">{{ hymnData['sub-title'] }}</h4>
       <h4>(R{{ hymnData.languages.russian }}, A{{ hymnData.languages.english }})</h4>
     </div>
-    <audio class="hymn-mp3" controls="" autostart="false" preload ="none" name="media">
-      <source src="https://www.hymnal.net/Hymns/Hymnal/mp3/e0007_full.mp3" type="audio/mpeg">
+    <audio v-if="hymnData['melody-url']" class="hymn-mp3" controls="" autostart="false" preload="none" name="media">
+      <source v-bind:src="hymnData['melody-url']" type="audio/mpeg">
     </audio>
     <div class="verses">
       <ol class="verse-one">
