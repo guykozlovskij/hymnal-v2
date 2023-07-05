85585b05386664bd992667f62a1c69cef264f29f
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-07-05 12:30:26 +0100

    include audio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,7 @@ export default {
     <section id="home-view">
       <h1>Himnynas</h1>
       <div id="hymn-list">
-        <div v-for="hymn in hymnalData" :key="hymn.number">
+        <div v-for="(hymn, index) in hymnalData" :key="index">
           <button class="hymn-select-button" @click="$router.push(`/hymns/${hymn.number}`)" role="link">{{ hymn.number
           }}</button>
         </div>
--- src/views/HymnView.vue
+++ src/views/HymnView.vue
@@ -15,12 +15,15 @@ export default {
 
 <template>
   <section class="hymn-view">
-    <div id="hymn-intro">
+    <div class="hymn-intro">
       <h4>{{ hymnData.number }}. {{ hymnData.title }}</h4>
       <h4 v-if="hymnData['sub-title']">{{ hymnData['sub-title'] }}</h4>
       <h4>(R{{ hymnData.languages.russian }}, A{{ hymnData.languages.english }})</h4>
     </div>
-    <div id="verses">
+    <audio class="hymn-mp3" controls="" autostart="false" preload ="none" name="media">
+      <source src="https://www.hymnal.net/Hymns/Hymnal/mp3/e0007_full.mp3" type="audio/mpeg">
+    </audio>
+    <div class="verses">
       <ol class="verse-one">
         <li class="hymn-verse">
           <span v-for="(line, index) in hymnData.verses[0]" :key="index">
@@ -37,7 +40,7 @@ export default {
       </div>
       <ol class="remaining-verses" start="2">
         <li class="hymn-verse" v-for="(verse, index) in hymnData.verses.slice(1)" :key="index">
-          <span v-for="line in verse" :key="line.id">
+          <span v-for="(line, index) in verse" :key="index">
             {{ line }}
             <br>
           </span>
