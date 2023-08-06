aa5c40f2c36f791fc828a28c158ca32ae567d53d
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-08-06 17:36:28 +0100

    add up to 65

--- src/views/HymnView.vue
+++ src/views/HymnView.vue
@@ -19,9 +19,11 @@ export default {
       <h4 v-if="hymnData['sub-title']">{{ hymnData['sub-title'] }}</h4>
       <h4 v-if="hymnData['languages']">(R{{ hymnData.languages.russian }}, A{{ hymnData.languages.english }})</h4>
     </div>
+
     <audio v-if="hymnData['melody-url']" class="hymn-mp3" controls="" autostart="false" preload="none" name="media">
       <source v-bind:src="hymnData['melody-url']" type="audio/mpeg">
     </audio>
+
     <!--* Rendering an ordered list if there are multiple verses -->
     <div class="verses">
       <ol v-if="hymnData['verses'].length > 1" class="verse-one">
@@ -32,6 +34,7 @@ export default {
           </span>
         </li>
       </ol>
+
       <!--* Not rendering lists if there is only 1 verse -->
       <div v-if="hymnData['verses'].length === 1">
         <span v-for="(line, index) in hymnData.verses[0]" :key="index">
@@ -39,12 +42,14 @@ export default {
             <br>
           </span>
       </div>
+
       <div v-if="hymnData.chorus" class="hymn-chorus">
         <span v-for="(line, index) in hymnData.chorus" :key="index">
           {{ line }}
           <br>
         </span>
       </div>
+
       <ol class="remaining-verses" start="2">
         <li class="hymn-verse" v-for="(verse, index) in hymnData.verses.slice(1)" :key="index">
           <span v-for="(line, index) in verse" :key="index">
@@ -53,6 +58,7 @@ export default {
           </span>
         </li>
       </ol>
+      
     </div>
   </section>
 </template>        
