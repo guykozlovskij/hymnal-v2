17b8d076bcfa16b5284bf651036055110a87b3fe
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-11-28 09:32:22 +0000

    basic nav color

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/"><v-icon name="bi-book" scale="2"/></RouterLink>
-        <RouterLink to="/"><v-icon name="bi-search" scale="2"/></RouterLink>
-        <RouterLink to="/"><v-icon name="bi-gear-wide" scale="2"/></RouterLink>
+        <RouterLink to="/"><v-icon class="vue-icon" name="bi-book" scale="2" style="color: transparent;"/></RouterLink>
+        <RouterLink to="/"><v-icon class="vue-icon" name="bi-search" scale="2"/></RouterLink>
+        <RouterLink to="/"><v-icon class="vue-icon" name="bi-gear-wide" scale="2"/></RouterLink>
       </nav>
     </div>
   </header>
