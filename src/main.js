bb6215dc52410b2140a62c90e29ece89684c4f1c
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2023-12-12 08:01:52 +0000

    add base dark mode

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import './assets/style.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { BiBook, BiSearch, BiStar, BiGearWide } from "oh-vue-icons/icons";
-addIcons(BiBook, BiSearch, BiStar, BiGearWide);
+import { OhVueIcon, addIcons } from "oh-vue-icons"
+import { BiBook, BiSearch, BiStar, BiGearWide } from "oh-vue-icons/icons"
+addIcons(BiBook, BiSearch, BiStar, BiGearWide)
 
 const app = createApp(App)
 
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,14 +1,21 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+</script>
+
 <script>
-export default {
-  data() {
-    return {
-    }
-  },
-}
+const isDark = useDark({
+  selector: "body", //element to add attribute to
+  attribute: "theme", // attribute name
+  valueDark: "custom-dark", // attribute value for dark mode
+  valueLight: "custom-light", // attribute value for light mode
+});
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
   <section>
     <h1>Settings</h1>
+    <p>Dark theme {{ isDark }}</p>
+    <button @click="toggleDark()">Toggle Color Mode</button>
   </section>
 </template>
