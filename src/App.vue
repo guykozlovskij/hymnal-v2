c54251182ca0a6fe6e0bd480b09767c04f2c740e
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2024-03-21 10:27:07 +0000

    trigger vercel build

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <RouterView />
     <div class="wrapper">
       <nav>
-        <RouterLink to="/"><v-icon class="vue-icon" name="bi-book" scale="2.2" /></RouterLink>
-        <RouterLink to="/search"><v-icon class="vue-icon" name="bi-search" scale="2.2" /></RouterLink>
-        <RouterLink to="/settings"><v-icon class="vue-icon" name="bi-gear-wide" scale="2.2" /></RouterLink>
+        <RouterLink to="/"><v-icon class="vue-icon" name="bi-book" scale="2.2"/></RouterLink>
+        <RouterLink to="/search"><v-icon class="vue-icon" name="bi-search" scale="2.2"/></RouterLink>
+        <RouterLink to="/settings"><v-icon class="vue-icon" name="bi-gear-wide" scale="2.2"/></RouterLink>
       </nav>
     </div>
   </header>
