0ec5f63267d826d5298a5188f3c739a3fc2cca2b
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2024-03-21 11:20:44 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
   plugins: [
     basicSsl(),
@@ -15,7 +16,7 @@ export default defineConfig({
         short_name: "Himnynas",
         icons: [
           {
-            src: './public/icons/icon.png',
+            src: '/icons/icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
