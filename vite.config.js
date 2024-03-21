a39bbc3c59df0435e3fce02069ed67d8ec8489bb
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2024-03-21 11:32:09 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         short_name: "Himnynas",
         icons: [
           {
-            src: '/public/icons/icon.png',
+            src: '/icons/icon.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
